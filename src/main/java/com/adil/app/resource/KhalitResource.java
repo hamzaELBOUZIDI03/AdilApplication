0c6ff53ccd2893601f969112035b7a30ee4eabd6
apachi1444 <yessinejawa@gmail.com>
2025-07-03 23:45:18 +0100

    add cross origin for new front url for tables coffre fort

--- src/main/java/com/adil/app/resource/KhalitResource.java
+++ src/main/java/com/adil/app/resource/KhalitResource.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RequestMapping("/api/v1/khalits")
 @RequiredArgsConstructor
 @Slf4j
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000","https://v0-simple-ai-ui-design.vercel.app/"} )
 public class KhalitResource {
 
     private final KhalitService khalitService;
